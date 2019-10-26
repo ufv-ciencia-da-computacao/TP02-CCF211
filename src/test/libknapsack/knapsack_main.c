2c2e59c718190b84b9088f77749de9aacbc5b447
Germano Barcelos <gegenbarcelos@gmail.com>
2019-10-26 19:12:22 -0300

    read from file finished

--- src/test/libknapsack/knapsack_main.c
+++ src/test/libknapsack/knapsack_main.c
@@ -5,7 +5,14 @@
 
 int main() {
   int N;
-  tuple_t *arr, *max_subset_arr;
+  tuple_t *arr;
 
+  tuple_arr_init(&arr, get_lines_file(INPUT_1)-1);
+  read_from_file(&arr, &N, INPUT_1);
+  
+  printf("N: %d\n", N);
 
+  for (int i = 0; i < 10; i++) {
+    printf("Id: %d | weight: %d\n", arr[i].id, arr[i].weight);
+  }
 }
