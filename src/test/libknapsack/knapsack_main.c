03c5386fe78b4daffab54c0bda1d8756dc558945
Germano Barcelos <gegenbarcelos@gmail.com>
2019-10-27 10:34:10 -0300

    adding inputfiles

--- src/test/libknapsack/knapsack_main.c
+++ src/test/libknapsack/knapsack_main.c
@@ -1,20 +1,25 @@
 #include "../../libknapsack/includes/knapsack.h"
 #include "../../libinterface/includes/file.h"
 
-#define INPUT_1 "../../input/1.txt"
+#include <time.h>
+
+#define INPUT_10 "../../input/10.txt"
+#define INPUT_50 "../../input/50.txt"
+#define INPUT_80 "../../input/80.txt"
+#define INPUT_100 "../../input/100.txt"
 
 int main() {
-  int N, lines = get_lines_file(INPUT_1)-1;
+  int C, N = get_lines_file(INPUT_50)-1;
   tuple_t *arr;
 
-  tuple_arr_init(&arr, lines);
-  read_from_file(&arr, &N, INPUT_1);
+  tuple_arr_init(&arr, N);
+  read_from_file(&arr, &C, INPUT_50);
 
   printf("-------read from file-------\n");
   
-  printf("N: %d\n", N);
+  printf("C: %d\n", C);
 
-  for (int i = 0; i < get_lines_file(INPUT_1)-1; i++) {
+  for (int i = 0; i < N-1; i++) {
     printf("Weight: %d | Value: %d\n", arr[i].weight, arr[i].value);
   }  
 
@@ -23,7 +28,15 @@ int main() {
 
   printf("\n-------knapsack algorithm-------\n");
 
-  knapsack_algorithm(arr, &maxSubsetArr, lines, N);
+  clock_t t;
+  t = clock();
+
+  knapsack_algorithm(arr, &maxSubsetArr, N, C);
+
+  t = clock() - t; 
+  double time_taken = ((double)t)/CLOCKS_PER_SEC;
+
+  printf("knapsack_algorithm() took %f seconds to execute \n", time_taken);
 
   for (int i = 0; i < N; i++) {
     if (maxSubsetArr[i].weight != 0) {
